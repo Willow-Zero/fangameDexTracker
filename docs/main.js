09a718ffd439b6a30be3e0f809fe9e59b7669ed0
Willow-Zero <zmckinnis4@gmail.com>
2024-01-13 13:49:09 -0500

    Update main.js

--- docs/main.js
+++ docs/main.js
@@ -1,4 +1,4 @@
-dexJSONURL=""
+dexJSONURL="https://raw.githubusercontent.com/ZerosLab/fangameDexTracker/main/docs/dex.json"
 
 newtable="<table><tr>"
 
@@ -7,26 +7,39 @@ const urlParams = new URLSearchParams(queryString);
 const game = urlParams.get('game');
 const type = urlParams.get('type');
 const level = urlParams.get('level');
-i = 0;
+inc = 0;
 fullDexURL = "JSONDEX_" + game + "_" + type + "_" + level + ".json"
-
+fullDexURL = "https://raw.githubusercontent.com/ZerosLab/fangameDexTracker/main/docs/JSONDEX_Uranium_Living_Regional.json"
 $.getJSON(fullDexURL, function(Data){
-
-console.log(Data);
-$.getJSON(dexJSONURL, function(dex)){
-  console.log(dex);
-
-  for element in Data{
-    i+=1;
+//console.log(Data);
+$.getJSON(dexJSONURL, function(dex){
+ // console.log(dex);
+ newtable += "<tr><td colspan=6> 0-30 </tr>"
+  for (var element in Data){
+		inc += 1;
+    i = String(inc).padStart(3, '0');
     newtable += "<td";
-    if element.name in dex:
-      newtable +=' style="background-color:yellow"> ';
-    else:
-      newtable +=">";
-      newtable += "</td>"
-    if i%6=0:
-      newtable+="</tr><tr>"
-    if i%30 = 0:
-      newtable += "<tr><tr><td colspan=6> " + i.toString + "-" + (i+30).tostring + "</tr><tr>"
-document.getElementById("p1").innerHTML = newtable;
-  })}
+    if (dex[i]){
+      newtable +=' style="background-color:yellow"> ';}
+    else{
+      newtable +=">";}
+    newtable += i + " - " + Data[i]["Name"] + "<br><img src="+i+"_"+Data[i]["Name"]+".png>" + "<br><img src=" + Data[i]["Type"]["Primary"] + ".png>";
+    if (Data[i]["Type"]["Secondary"]){
+    	newtable += "<img src=" + Data[i]["Type"]["Secondary"] + ".png>";
+    }
+    newtable += "<br>" +Data[i]["Obtaining"];
+    if (Data[i]["Evolutions"]["1"]){
+    	newtable += "<br> Evolutions:<br>" + Data[i]["Evolutions"]["1"];
+    }
+     if (Data[i]["Preevoluitons"]["1"]){
+    	newtable += "<br> Preevolutions:<br>" + Data[i]["Preevoluitons"]["1"];
+    }
+    newtable += "</td>";
+    if (i%6==0){
+      newtable+="</tr><tr>"}
+    if (i%30 == 0){
+      newtable += "</tr><tr><td colspan=6> " + i + "-" + (i+30) + "</tr><tr>"}
+    
+  }
+  document.getElementById("dex").innerHTML = newtable;
+  })})
